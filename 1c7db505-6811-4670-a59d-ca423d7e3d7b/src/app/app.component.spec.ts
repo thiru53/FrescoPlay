22a390826aaacec3172cf5f5ab7397c42263b8a5
Tiru <thiru53@gmail.com>
2023-08-14 12:28:52 +0530

    Angular_HealthCase Application

---
+++ 1c7db505-6811-4670-a59d-ca423d7e3d7b/src/app/app.component.spec.ts
@@ -0,0 +1,5 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
